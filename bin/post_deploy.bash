e15b0816badb2330e17c1226931f02623bae5dbc
Miroslav Schlossberg <mschlos@gmail.com>
2017-11-19 19:23:40 +0100

    # Conflicts: #	2016/cities.html #	2016/democracy.html #	2016/foi.html

--- bin/post_deploy.bash
+++
@@ -1,6 +0,0 @@
-cd "$(dirname $BASH_SOURCE)"/..
-
-ruby1.9.1 /usr/bin/bundle install --deployment --binstubs ../gem-bin
-cd $1
-../../gem-bin/sass --update assets/sass:assets/css --style compressed
-../../gem-bin/jekyll build
